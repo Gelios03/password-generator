56c18d677e70ce98c694866eba0962f1076f771d
Sergey <sergogenius@yandex.ru>
2025-09-26 22:57:57 +0500

    Настроить генерируемые пароли

--- password-generator.py
+++ password-generator.py
@@ -15,3 +15,19 @@ include_uppercase = input('Включать прописные буквы? (да
 include_lowercase = input('Включать строчные буквы? (да / нет) ')
 include_symbols = input('Включать символы? (да / нет) ')
 include_ambiguous = input('Исключить неоднозначные символы: «il1Lo0O»? (да / нет) ')
+
+if include_digits.lower() == 'да':
+    chars += DIGITS
+if include_uppercase.lower() == 'да':
+    chars += UPPERCASE
+if include_lowercase.lower() == 'да':
+    chars += LOWERCASE
+if include_symbols.lower() == 'да':
+    chars += SYMBOLS
+if include_ambiguous.lower() == 'да':
+    for char in AMBIGUOUS:
+        chars = chars.replace(char, '')
+
+if not chars:
+    print('Ошибка: требуется выбрать как минимум один тип символов!')
+    exit()
