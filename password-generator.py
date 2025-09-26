3c0f2728d5ee352b8bebdc2a6f68e61a39d11567
Sergey <sergogenius@yandex.ru>
2025-09-26 22:26:20 +0500

    Добавлен заголовок программы

---
+++ password-generator.py
@@ -0,0 +1,9 @@
+import random
+
+DIGITS = '0123456789'
+UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+SYMBOLS = '!#$%&*+-=?@^_'
+AMBIGUOUS = 'il1Lo0O'
+
+chars = ''
