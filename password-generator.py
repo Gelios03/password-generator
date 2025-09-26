1388851e11c21962919e6babd73138c4e000f16b
Sergey <sergogenius@yandex.ru>
2025-09-26 22:45:26 +0500

    Реализовать ввод пользовательских данных

--- password-generator.py
+++ password-generator.py
@@ -7,3 +7,11 @@ SYMBOLS = '!#$%&*+-=?@^_'
 AMBIGUOUS = 'il1Lo0O'
 
 chars = ''
+
+num_passwords = int(input('Количество паролей: '))
+length_password = int(input('Длина пароля: '))
+include_digits = input('Включать цифры? (да / нет) ')
+include_uppercase = input('Включать прописные буквы? (да / нет) ')
+include_lowercase = input('Включать строчные буквы? (да / нет) ')
+include_symbols = input('Включать символы? (да / нет) ')
+include_ambiguous = input('Исключить неоднозначные символы: «il1Lo0O»? (да / нет) ')
